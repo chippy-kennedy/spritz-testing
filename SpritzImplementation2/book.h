1538636d4307fa4ab86c60456c71332e0ad7130f
Chippy Kennedy <ckennedy@overdrive.com>
2014-06-09 17:22:36 -0400

    Added Multiple Text capability to both WebView and NativeView. Both Work by pulling the native pList data.

---
+++ SpritzImplementation2/book.h
@@ -0,0 +1,13 @@
+//
+//  book.h
+//  SpritzImplementation2
+//
+//  Created by Chippy Kennedy on 6/9/14.
+//  Copyright (c) 2014 Overdrive Media. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface book : NSObject
+
+@end
---
+++ SpritzImplementation2/book.m
@@ -0,0 +1,13 @@
+//
+//  book.m
+//  SpritzImplementation2
+//
+//  Created by Chippy Kennedy on 6/9/14.
+//  Copyright (c) 2014 Overdrive Media. All rights reserved.
+//
+
+#import "book.h"
+
+@implementation book
+
+@end
