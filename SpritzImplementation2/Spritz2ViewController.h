b560f4b68ee331f4a8578c132f2dd8823794fbcc
Chippy Kennedy <ckennedy@overdrive.com>
2014-06-09 12:55:03 -0400

    Added Spritz Capabilities to Native and Web groups. Both have functionality on a single text example. The Web View pulls a URL on the spritz and does not use the web view for spritz

---
+++ SpritzImplementation2/Spritz2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Spritz2ViewController.h
+//  SpritzImplementation2
+//
+//  Created by Chippy Kennedy on 6/9/14.
+//  Copyright (c) 2014 Overdrive Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Spritz2ViewController : UIViewController
+
+@end
