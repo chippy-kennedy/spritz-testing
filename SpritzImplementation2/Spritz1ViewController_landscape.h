e1841706b429fd3c19579c08c697497b0ca9f3be
Chippy Kennedy <ckennedy@overdrive.com>
2014-06-10 10:51:46 -0400

    Finished landscape implementation. Added unique classes to landscape views and a toggle button between portrait and landscape on the redicle viewController

---
+++ SpritzImplementation2/Spritz1ViewController_landscape.h
@@ -0,0 +1,13 @@
+//
+//  Spritz1ViewController_landscape.h
+//  SpritzImplementation2
+//
+//  Created by Chippy Kennedy on 6/10/14.
+//  Copyright (c) 2014 Overdrive Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Spritz1ViewController_landscape : UIViewController
+
+@end
---
+++ SpritzImplementation2/Spritz1ViewController_landscape.m
@@ -0,0 +1,49 @@
+//
+//  Spritz1ViewController_landscape.m
+//  SpritzImplementation2
+//
+//  Created by Chippy Kennedy on 6/10/14.
+//  Copyright (c) 2014 Overdrive Media. All rights reserved.
+//
+
+#import "Spritz1ViewController_landscape.h"
+
+@interface Spritz1ViewController_landscape ()
+
+@end
+
+@implementation Spritz1ViewController_landscape
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
